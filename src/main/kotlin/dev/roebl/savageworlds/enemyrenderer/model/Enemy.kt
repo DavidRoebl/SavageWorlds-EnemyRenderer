f6b585b2ee49ddec55e162599cfd0177f178d4da
roebsi <31534652+DavidRoebl@users.noreply.github.com>
2023-08-04 12:34:25 +0200

    first draft: done

--- src/main/kotlin/dev/roebl/savageworlds/enemyrenderer/model/Enemy.kt
+++ src/main/kotlin/dev/roebl/savageworlds/enemyrenderer/model/Enemy.kt
@@ -9,18 +9,17 @@ data class Enemy(
     val pace: String,
     val parry: String,
     val toughness: String,
-    val powers: Array<String>,
-    val equipment: Array<String>,
+    val powers: Map<String, String>,
+    val equipment: Map<String, String>,
     val isWildcard: Boolean,
-    val notes: Array<String>,
+    val notes: Map<String, String>,
     val salvage: Salvage,
-    val hindrances: Array<String>,
-    val edges: Array<String>
+    val hindrances: Map<String, String>,
+    val edges: Map<String, String>
 ) {
 
     val sortedSkills: Map<String, Die>
         get() = skills.toSortedMap(baseSkillsFirst andThen byNaturalOrder)
-
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -34,11 +33,13 @@ data class Enemy(
         if (pace != other.pace) return false
         if (parry != other.parry) return false
         if (toughness != other.toughness) return false
-        if (!powers.contentEquals(other.powers)) return false
-        if (!equipment.contentEquals(other.equipment)) return false
+        if (powers != other.powers) return false
+        if (equipment != other.equipment) return false
         if (isWildcard != other.isWildcard) return false
-        if (!notes.contentEquals(other.notes)) return false
+        if (notes != other.notes) return false
         if (salvage != other.salvage) return false
+        if (hindrances != other.hindrances) return false
+        if (edges != other.edges) return false
 
         return true
     }
@@ -50,12 +51,16 @@ data class Enemy(
         result = 31 * result + pace.hashCode()
         result = 31 * result + parry.hashCode()
         result = 31 * result + toughness.hashCode()
-        result = 31 * result + powers.contentHashCode()
-        result = 31 * result + equipment.contentHashCode()
+        result = 31 * result + powers.hashCode()
+        result = 31 * result + equipment.hashCode()
         result = 31 * result + isWildcard.hashCode()
-        result = 31 * result + notes.contentHashCode()
+        result = 31 * result + notes.hashCode()
         result = 31 * result + salvage.hashCode()
+        result = 31 * result + hindrances.hashCode()
+        result = 31 * result + edges.hashCode()
         return result
     }
+
+
 }
 
--- src/main/kotlin/dev/roebl/savageworlds/enemyrenderer/pdf/PdfBoxRenderer.kt
+++ src/main/kotlin/dev/roebl/savageworlds/enemyrenderer/pdf/PdfBoxRenderer.kt
@@ -29,7 +29,6 @@ class PdfBoxRenderer : PdfRenderer {
 
             PDPageContentStream(document, page).use { stream ->
                 val matrix = Matrix.getTranslateInstance(0f, height)
-//                val matrix = Matrix.getRotateInstance(2* PI, 0f, 0f).multiply(Matrix.getTranslateInstance(0f, height))
                 stream.transform(matrix)
                 if (enemy.isWildcard) {
                     val image = PDImageXObject.createFromFile(icWildcard, document)
@@ -40,7 +39,6 @@ class PdfBoxRenderer : PdfRenderer {
                     textSize = 24f,
                     xPos = if (enemy.isWildcard) 42f else 10f,
                     yPos = 10f
-//                    yPos = 34f
                 )
 
                 stream.smallText(content = "Salvage", xPos = width - 15, yPos = 13f, align = RIGHT)
@@ -52,24 +50,62 @@ class PdfBoxRenderer : PdfRenderer {
                 // column 1
                 var yOffset = BODY_START
                 var xOffset = 15f
-                yOffset += stream.smallText(content = "Attributes", xPos = xOffset, yPos = yOffset)//, yPos = height - 60)
-                yOffset += stream.renderMap(enemy.attributes.asMap(), xPos1 = xOffset, xPos2 = xOffset+50, yPos =  yOffset)//, yStart = height - 74)
-
+                yOffset += stream.smallText("Attributes", xPos = xOffset, yPos = yOffset)
+                yOffset += stream.dieMap(
+                    enemy.attributes.asMap(),
+                    xPos1 = xOffset,
+                    xPos2 = xOffset + 50,
+                    yPos = yOffset
+                )
                 yOffset += SECTION_SPACE
-                yOffset += stream.smallText(content = "Skills", xPos = xOffset, yPos =  yOffset)//, yPos = height - 147)
-                yOffset += stream.renderMap(enemy.sortedSkills, xPos1 = xOffset, xPos2 = xOffset+50, yPos =  yOffset)//, yStart = height - 161)
+
+                if (enemy.skills.isNotEmpty()) {
+                    yOffset += stream.smallText("Skills", xPos = xOffset, yPos = yOffset)
+                    yOffset += stream.dieMap(
+                        enemy.sortedSkills,
+                        xPos1 = xOffset,
+                        xPos2 = xOffset + 50,
+                        yPos = yOffset
+                    )
+                    yOffset += SECTION_SPACE
+                }
 
                 // column 2
                 yOffset = BODY_START
                 xOffset = 100f
-                yOffset +=stream.smallText("Equipment", xPos = xOffset, yPos =  yOffset)// yPos =  60f)
-                yOffset +=stream.textAll(enemy.equipment, xPos = xOffset, yPos =  yOffset)// yPos =  74f)
+                if (enemy.equipment.isNotEmpty()) {
+                    yOffset += stream.smallText("Equipment", xPos = xOffset, yPos = yOffset)
+                    yOffset += stream.annotatedList(enemy.equipment, xPos = xOffset, yPos = yOffset)
+                    yOffset += SECTION_SPACE
+                }
 
-                yOffset += SECTION_SPACE
-                yOffset += stream.smallText("Powers", xPos = xOffset, yPos = yOffset)
+                if (enemy.powers.isNotEmpty()) {
+                    yOffset += stream.smallText("Powers", xPos = xOffset, yPos = yOffset)
+                    yOffset += stream.annotatedList(enemy.powers, xPos = xOffset, yPos = yOffset)
+                    yOffset += SECTION_SPACE
+                }
+
+                // column 3
+                yOffset = BODY_START
+                xOffset = width - 160
+                if (enemy.hindrances.isNotEmpty()) {
+                    yOffset += stream.smallText("Hindrances", xPos = xOffset, yPos = yOffset)
+                    yOffset += stream.annotatedList(enemy.hindrances, xPos = xOffset, yPos = yOffset)
+                    yOffset += SECTION_SPACE
+                }
+                if (enemy.edges.isNotEmpty()) {
+                    yOffset += stream.smallText("Edges", xPos = xOffset, yPos = yOffset)
+                    yOffset += stream.annotatedList(enemy.edges, xPos = xOffset, yPos = yOffset)
+                    yOffset += SECTION_SPACE
+                }
+                if (enemy.notes.isNotEmpty()) {
+                    yOffset += stream.smallText("Notes", xPos = xOffset, yPos = yOffset)
+                    yOffset += stream.annotatedList(enemy.notes, xPos = xOffset, yPos = yOffset)
+                    yOffset += SECTION_SPACE
+                }
 
 
-
+                stream.rectangle(xPos = width - 200, yPos = height - 38, width = 10f, height = 10f)
 
                 stream.smallText(content = "Pace", xPos = width - 95, yPos = height - 38f, align = RIGHT)
                 stream.text(content = enemy.pace, xPos = width - 95, yPos = height - 27f, align = RIGHT)
@@ -83,10 +119,46 @@ class PdfBoxRenderer : PdfRenderer {
         }
     }
 
-    private fun PDPageContentStream.renderMap(
+    private fun PDPageContentStream.annotatedList(
+        map: Map<String, String>,
+        yPos: Float,
+        xPos: Float,
+        maxWidth: Float = 150f
+    ): Float {
+
+        val font = font()
+        beginText()
+        setLeading(15f)
+        newLineAtOffset(xPos, -1 * yPos - 12f)
+        var overflows = 0
+        map.forEach { entry, annotation ->
+            val width = with(font) {
+                widthOf("$entry ", 12f) + widthOf(annotation, 9f)
+            }
+
+            setFont(font, 12f)
+            showText("$entry ")
+            if (width > maxWidth) {
+                setLeading(10f)
+                newLine()
+                setLeading(14f)
+                overflows += 1
+                showText("  ")
+            }
+
+            setFont(font, 9f)
+            showText(annotation)
+            newLine()
+        }
+        endText()
+        return map.size * 14f + overflows * 10f
+    }
+
+    private fun PDPageContentStream.dieMap(
         map: Map<String, Die>,
         isBold: Boolean = false,
         textSize: Float = 12f,
+        textSize2: Float = 12f,
         lineSpace: Float = 2f,
         yPos: Float,
         xPos1: Float,
@@ -94,61 +166,28 @@ class PdfBoxRenderer : PdfRenderer {
     ): Float {
         var _yPos = yPos
 
-        map.forEach { (attribute, die) ->
+        map.forEach { (key, value) ->
             text(
-                content = attribute.replaceFirstChar { it.uppercaseChar() },
+                content = key.replaceFirstChar { it.uppercaseChar() },
                 xPos = xPos1,
                 yPos = _yPos,
                 align = LEFT,
                 textSize = textSize,
+                lineSpace = lineSpace,
                 isBold = isBold
             )
             text(
-                content = die.name,
+                content = value.name,
                 xPos = xPos2,
                 yPos = _yPos,
                 align = LEFT,
-                textSize = textSize,
+                textSize = textSize2,
+                lineSpace = lineSpace,
                 isBold = isBold
             )
             _yPos += textSize + lineSpace
         }
         return _yPos - yPos
-    }
-
-
-    private fun PDPageContentStream.textAll(
-        content: Array<String>,
-        textSize: Float = 12f,
-        lineSpace: Float = 2f,
-        isBold: Boolean = false,
-        xPos: Float,
-        yPos: Float
-    ): Float {
-        val strings = content.map { it.trim() }
-            .map { it.split("\n").map { it.trim() } }
-            .map {
-                if (it.size > 1) {
-                    val mutable = it.toMutableList()
-                    mutable[1] = mutable[1].prependIndent(HANGING_INDENT)
-                    mutable
-                } else {
-                    it
-                }
-            }
-            .flatten()
-
-        val font = font(isBold)
-        beginText()
-        setFont(font, textSize)
-        setLeading(textSize + lineSpace)
-        newLineAtOffset(xPos, -1 * yPos - textSize)
-        strings.forEach {
-            showText(it)
-            newLine()
-        }
-        endText()
-        return strings.size * (textSize + lineSpace)
     }
 
     private fun PDPageContentStream.smallText(
@@ -157,7 +196,7 @@ class PdfBoxRenderer : PdfRenderer {
         xPos: Float,
         yPos: Float
     ): Float {
-        return text(content, textSize = 9f, lineSpace = 2f, align = align, xPos = xPos, yPos = yPos)
+        return text(content, textSize = 9f, isBold = true, lineSpace = 2f, align = align, xPos = xPos, yPos = yPos)
     }
 
     private fun PDPageContentStream.text(
@@ -194,6 +233,19 @@ class PdfBoxRenderer : PdfRenderer {
         return strings.size * (textSize + lineSpace)
     }
 
+    private fun PDPageContentStream.rectangle(xPos: Float, yPos: Float, width: Float, height: Float) {
+        val _xPos = xPos * -1
+        moveTo(_xPos, yPos)
+        stroke()
+        lineTo(_xPos + width, yPos)
+        stroke()
+        lineTo(_xPos + width, yPos + height)
+        stroke()
+        lineTo(_xPos, yPos + height)
+        stroke()
+        lineTo(_xPos, yPos)
+        stroke()
+    }
     private fun PDPageContentStream.horizontalRule(yPos: Float) {
         line(0f, yPos, width, yPos)
     }
@@ -216,5 +268,5 @@ class PdfBoxRenderer : PdfRenderer {
         return replaceFirstChar { it.uppercaseChar() }
     }
 
-    fun font(isBold: Boolean): PDType1Font = if (isBold) PDType1Font.HELVETICA_BOLD else PDType1Font.HELVETICA
+    fun font(isBold: Boolean = false): PDType1Font = if (isBold) PDType1Font.HELVETICA_BOLD else PDType1Font.HELVETICA
 }
